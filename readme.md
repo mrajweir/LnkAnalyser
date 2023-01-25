440614d254858c8101325d0fc66c9d1cde9331bf
Andrew Weir <mrajweir@gmail.com>
2023-01-25 06:33:21 +0000

    Adding a shortcut that can be used in a unit test. Not the right approach but gets it off the ground.

--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # LnkAnalyser
-A Python module for the forensic analysis of Windows shortcuts (they're surYpprisingly useful.) This package is parses
-shortcuts against the [Windows MS-SHLLINK standard](https://winprotocoldoc.blob.core.windows.net/productionwindowsarchives/MS-SHLLINK/%5bMS-SHLLINK%5d.pdf]).
+A Python module for the forensic analysis of Windows shortcuts (they're surprisingly useful.) This package is parses
+shortcuts against the [Windows MS-SHLLINK standard](https://learn.microsoft.com/en-us/openspecs/windows_protocols/ms-shllink/16cb4ca1-9339-4d0c-a68d-bf1d6cc0f943]).
 
 ## Usage
 
--- tests/test_LnkAnalyser.py
+++ tests/test_LnkAnalyser.py
@@ -1,9 +1,25 @@
 import unittest
+from LnkAnalyser import lnkanalyser
 
 class TestLnkAnalyser(unittest.TestCase):
 
     def test_Init(self):
         self.assertEqual('foo'.upper(), 'FOO')
 
-    def travis_ci_test(self):
-        self.assertEqual(1, 1)
+    def test_shortcut(self):
+            shortcut = lnkanalyser.go("Desktop.lnk")
+
+            print(shortcut["header_size"])
+            print(shortcut["link_class_id"])
+            print(shortcut["link_flags"])
+            print(shortcut["file_attributes"])
+            print(shortcut["creation_time_of_link_target"])
+            print(shortcut["access_time_of_link_target"])
+            print(shortcut["write_time_of_link_target"])
+            print(shortcut["target_file_size"])
+            print(shortcut["icon_index"])
+            print(shortcut["expected_window_state"])
+            print(shortcut["hot_key"])
+            print(shortcut["reserved_one"])
+            print(shortcut["reserved_two"])
+            print(shortcut["reserved_three"])

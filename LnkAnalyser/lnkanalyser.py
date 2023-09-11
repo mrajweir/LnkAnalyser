dde5feead421a37a12f51eb5bcd84ac9702814b0
Andrew Weir <andrew@pockettutor.cc>
2023-09-11 22:06:55 +0100

    Added in basic parsing for the LinkFlags structure.

--- LnkAnalyser/lnkanalyser.py
+++ LnkAnalyser/lnkanalyser.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-
+import pprint
 
 def convert_to_readable_date(hex_timestamp):
     # number of nanoseconds since january 1st, 1601
@@ -22,7 +22,7 @@ def get_byte_counter():
     return counters["bytes"]
 
 
-def go(path, endian="big"):
+def go(path, endian="little"):
     """Given a path to a Windows shortcut, return a dict() containing the properties of the shortcut
 
     :param path: The path of the Windows shortcut to load
@@ -103,4 +103,51 @@ def go(path, endian="big"):
             for key, value in properties.items():
                 value.reverse()
 
+    pprint.pprint(parseLinkHeaderFlags(properties["link_flags"]))
+
     return properties
+
+
+def parseLinkHeaderFlags(link_flags):
+    byte_data = bytes.fromhex(''.join(link_flags))
+
+    link_header_flags = dict()
+
+    link_header_flags["has_link_target_id_list"] = bool(byte_data[0] & (1 << 0) != 0)
+    link_header_flags["has_link_info"] = bool(byte_data[0] & (1 << 1) != 0)
+    link_header_flags["has_name"] = bool(byte_data[0] & (1 << 2) != 0)
+    link_header_flags["has_relative_path"] = bool(byte_data[0] & (1 << 3) != 0)
+    link_header_flags["has_working_dir"] = bool(byte_data[0] & (1 << 4) != 0)
+    link_header_flags["has_arguments"] = bool(byte_data[0] & (1 << 5) != 0)
+    link_header_flags["has_icon_location"] = bool(byte_data[0] & (1 << 6) != 0)
+    link_header_flags["is_unicode"] = bool(byte_data[0] & (1 << 7) != 0)
+
+    link_header_flags["force_no_link_info"] = bool(byte_data[0] & (1 << 8) != 0)
+    link_header_flags["has_exp_string"] = bool(byte_data[0] & (1 << 9) != 0)
+    link_header_flags["run_in_separate_process"] = bool(byte_data[0] & (1 << 10) != 0)
+    link_header_flags["unused_1"] = bool(byte_data[0] & (1 << 11) != 0)
+    link_header_flags["has_drawin_id"] = bool(byte_data[0] & (1 << 12) != 0)
+    link_header_flags["run_as_user"] = bool(byte_data[0] & (1 << 13) != 0)
+    link_header_flags["has_exp_icon"] = bool(byte_data[0] & (1 << 14) != 0)
+    link_header_flags["no_pidl_alias"] = bool(byte_data[0] & (1 << 15) != 0)
+
+    link_header_flags["unused_2"] = bool(byte_data[0] & (1 << 16) != 0)
+    link_header_flags["run_with_shim_layer"] = bool(byte_data[0] & (1 << 17) != 0)
+    link_header_flags["force_no_link_track"] = bool(byte_data[0] & (1 << 18) != 0)
+    link_header_flags["enable_target_metadata"] = bool(byte_data[0] & (1 << 19) != 0)
+    link_header_flags["disable_link_path_tracking"] = bool(byte_data[0] & (1 << 20) != 0)
+    link_header_flags["disable_known_folder_tracking"] = bool(byte_data[0] & (1 << 21) != 0)
+    link_header_flags["disable_known_folder_alias"] = bool(byte_data[0] & (1 << 22) != 0)
+    link_header_flags["allow_link_to_link"] = bool(byte_data[0] & (1 << 23) != 0)
+
+    link_header_flags["unalias_on_save"] = bool(byte_data[0] & (1 << 24) != 0)
+    link_header_flags["prefer_environment_path"] = bool(byte_data[0] & (1 << 25) != 0)
+    link_header_flags["keep_local_id_list_for_unc_target"] = bool(byte_data[0] & (1 << 26) != 0)
+    link_header_flags["bit_27"] = bool(byte_data[0] & (1 << 27) != 0)
+    link_header_flags["bit_28"] = bool(byte_data[0] & (1 << 28) != 0)
+    link_header_flags["bit_29"] = bool(byte_data[0] & (1 << 29) != 0)
+    link_header_flags["bit_30"] = bool(byte_data[0] & (1 << 30) != 0)
+    link_header_flags["bit_31"] = bool(byte_data[0] & (1 << 31) != 0)
+
+    return link_header_flags
+
--- tests/test_LnkAnalyser.py
+++ tests/test_LnkAnalyser.py
@@ -7,19 +7,4 @@ class TestLnkAnalyser(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_shortcut(self):
-            shortcut = lnkanalyser.go("Desktop.lnk")
-
-            print(shortcut["header_size"])
-            print(shortcut["link_class_id"])
-            print(shortcut["link_flags"])
-            print(shortcut["file_attributes"])
-            print(shortcut["creation_time_of_link_target"])
-            print(shortcut["access_time_of_link_target"])
-            print(shortcut["write_time_of_link_target"])
-            print(shortcut["target_file_size"])
-            print(shortcut["icon_index"])
-            print(shortcut["expected_window_state"])
-            print(shortcut["hot_key"])
-            print(shortcut["reserved_one"])
-            print(shortcut["reserved_two"])
-            print(shortcut["reserved_three"])
+        shortcut = lnkanalyser.go("Desktop.lnk")
